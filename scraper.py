577028710e47e5ab56b0d7bd2551a2626f320661
Jason Christopher <jchristopher2448@gmail.com>
2023-01-10 15:25:01 -0600

    finished Lab 17 and Stretch Goal, need README

--- scraper.py
+++ scraper.py
@@ -1,31 +1,43 @@
-import requests
-# pip install requests
-from bs4 import BeautifulSoup
-# pip install beautifulsoup4
+import requests    # pip install requests
+from bs4 import BeautifulSoup    # pip install beautifulsoup4
+import re
+
+
+def helper(r):
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup.find_all(title="Wikipedia:Citation needed")
+
+
+def polish(text):
+    polished = re.sub(r"\[(.*?)\]", "", text)
+    return polished
 
 
 def get_citations_needed_count(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    needs = soup.find_all(title="Wikipedia:Citation needed")
-    print(len(needs))
+    needs = helper(requests.get(url))
+    print("Number of citations needed: ", len(needs))
 
 
 def get_citations_needed_report(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    needs = soup.find_all(title="Wikipedia:Citation needed")
+    needs = helper(requests.get(url))
     for need in needs:
-        print(need.find_parent("p").text)
+        text = polish(need.find_parent("p").text)
+        print(text)
+
+
+def get_citations_needed_by_section(url):
+    needs = helper(requests.get(url))
+    current_header = needs[0].find_previous("h3").text
+    print(polish(current_header) + "\n")
+    for need in needs:
+        if need.find_previous("h3").text != current_header:
+            current_header = need.find_previous("h3").text
+            print(polish(current_header) + "\n")
+        text = polish(need.find_parent("p").text)
+        print(text)
 
 
 if __name__ == '__main__':
     get_citations_needed_count("https://en.wikipedia.org/wiki/History_of_Mexico")
-    get_citations_needed_report("https://en.wikipedia.org/wiki/History_of_Mexico")
-
-
-# if __name__ == '__main__':
-#     url = "https://testing-www.codefellows.org/course-calendar/?filters=400:%20Advanced,code-python-401"
-#     response = requests.get(url)
-#     results = parse(response.text)
-#     print(results)
+    # get_citations_needed_report("https://en.wikipedia.org/wiki/History_of_Mexico")
+    get_citations_needed_by_section("https://en.wikipedia.org/wiki/History_of_Mexico")
